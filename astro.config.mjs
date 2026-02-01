84cbf75c43780bd4bf691d57ffb3a94a6ec2aedf
brahim-belghiti <brahim.belghiti@proton.me>
2026-02-01 21:22:50 +0100

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel";
 export default defineConfig({
   site: "https://astro-nano-demo.vercel.app",
   integrations: [mdx(), react(), sitemap(), tailwind()],
+  adapter: vercel(),
 });
